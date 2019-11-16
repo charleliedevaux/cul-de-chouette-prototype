73590cd9a8c7609018937d68f374b3ffd40516ad
charleliedevaux <charlelie.devaux@gmail.com>
2019-11-16 14:25:37 +0100

    Quick navigation fix, to be improved later

--- src/components/NewGame/index.js
+++ src/components/NewGame/index.js
@@ -6,28 +6,36 @@ import { resetScores } from 'src/store/scores/actions';
 import './styles.sass';
 
 
+// Strategy to read from global state
+const mapStateToProps = (state) => {
+  const { rollStatus } = state.game;
+  return { rollStatus };
+};
+
 // Strategy to write in global state
 const mapDispatchToProps = (dispatch, props) => {
   return {
-    handleClick: (event) => {
-      dispatch(resetScores());
+    handleClick: (rollStatus) => () => {
+      if (!rollStatus) {
+        dispatch(resetScores());
+      }
       props.history.push('/game');
     }
   };
 };
 
 // Presentational Component
-const NewGame = ({ handleClick }) => {
+const NewGame = ({ handleClick, rollStatus }) => {
   return <div className="new-game">
     <h2 className="new-game-title">S'que vous voulez qu'on se fasse un petit cul de chouette ?</h2>
     <p className="new-game-quote">"Allez, ça nous détendra !"</p>
-    <button type="button" className="start-button" onClick={handleClick}>C'est parti !</button>
+    <button type="button" className="start-button" onClick={handleClick(rollStatus)}>C'est parti !</button>
   </div>;
 };
 
 
 const NewGameContainer = withRouter(connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(NewGame));
 
